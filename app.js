5201f835e64f6b53deae11291db6d52696305dce
Malindu Pabasara <mpabasara11@gmail.com>
2023-11-14 01:30:40 +0530

    few changes on app.js

--- app.js
+++ app.js
@@ -11,14 +11,6 @@ app.use(express.json());
 
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://mpabasara11:zxasd@cluster0.sktjibw.mongodb.net/Vacations4You?retryWrites=true&w=majority";
-
-
-
-connectToDb();
-
-
-
 
 
 var authentication = require('./Routers/userAuth');
@@ -40,26 +32,7 @@ app.use('/backoffice',backOfficeStaff);
 app.use('/admin',admin);
 
 
-
-
-const server = app.listen(port,()=>{console.log(`api is listening at http://localhost:${port}`)});
-
-
-// Handle Ctrl+C to gracefully disconnect from the database
-process.on('SIGINT', async () => {
-    try {
-      // Close the MongoDB connection
-      await mongoose.connection.close();
-      console.log('MongoDB connection closed');
-    } catch (error) {
-      console.error('Error closing MongoDB connection:', error);
-    }
-    // Close the server
-    server.close(() => {
-      console.log('Server closed');
-      process.exit(0);
-    });
-  });
+connectToDb();
   
 
 //////////////////////////////////////////////////////////////   Function definitions ///////////////////////////////////////////////////////////////////////////////
@@ -68,11 +41,13 @@ process.on('SIGINT', async () => {
 //entablish the connection with database
 async function connectToDb()
 {
+  const uri = "mongodb+srv://mpabasara11:zxasd@cluster0.sktjibw.mongodb.net/Vacations4You?retryWrites=true&w=majority";
 
     try
     {
-        await mongoose.connect(uri);
+       await mongoose.connect(uri);
         console.log("Connected to mongoDB");
+        startServer();
     }
     catch(error)
     {
@@ -81,6 +56,13 @@ async function connectToDb()
 
     }
 
+}
+
+function startServer()
+{
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+    });
 }
 
 
